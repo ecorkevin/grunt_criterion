d2aec22e660a741568e19546ff1ec647dbb78ded
Kevin Moritz <kevinecor@gmail.com>
2013-08-22 12:35:53 -0500

    added default.js

--- default.js
+++ default.js
@@ -1,6 +1,7 @@
+/*jshint globalstrict: true*/
 "use strict";
 module.exports = {
   reporter: function(results, data, opts) {
     process.stdout.write('{"report":' + JSON.stringify(results, null, 2) + '}');
   }
-}
+};
